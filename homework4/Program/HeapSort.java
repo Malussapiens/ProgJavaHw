2d78b1058b62739fdee876cccc29a146b22a3fa0
Dead_Moroz <malussapiens@gmail.com>
2023-03-07 15:26:23 +0300

    Heapsort class modified

--- homework4/Program/HeapSort.java
+++ homework4/Program/HeapSort.java
@@ -5,20 +5,20 @@ package Homework4.Program;
  */
 public class HeapSort {
     public void sort(int arr[]) {
-        int n = arr.length;
+        int arrLen = arr.length;
 
-        // Построение кучи (перегруппируем массив)
-        for (int i = n / 2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
-
-        // Один за другим извлекаем элементы из кучи
-        for (int i = n - 1; i >= 0; i--) {
+        // Строим кучу
+        for (int i = arrLen / 2 - 1; i >= 0; i--)
+            heapify(arr, arrLen, i);
+        
+            // Берем элементы из кучи
+        for (int i = arrLen - 1; i >= 0; i--) {
             // Перемещаем текущий корень в конец
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
 
-            // Вызываем процедуру heapify на уменьшенной куче
+            // Вызываем heapify на уменьшенной куче
             heapify(arr, i, 0);
         }
     }
@@ -34,8 +34,8 @@ public class HeapSort {
         if (l < n && arr[l] > arr[largest])
             largest = l;
 
-        // Если правый дочерний элемент больше, чем самый большой элемент на данный
-        // момент
+        // Если правый дочерний элемент больше, чем самый большой элемент 
+        // на данный момент
         if (r < n && arr[r] > arr[largest])
             largest = r;
         // Если самый большой элемент не корень
